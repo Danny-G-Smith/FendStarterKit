c90f0a9f6a5c2b569df99d674232e7a35556c16e
Danny G Smith <dgs@campfirepixels.com>
2018-06-20 20:05:47 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Project: Memory Game
+## Front-End Web Developer Nanodegree Program
+
+This game challenges you to work against the clock and match as many squares as you can before the time runs out.
+
+You can play the game in your browser by [clicking here](https://danny-g-smith.github.io/Matching-Game/).
+
+## How to Play
+
+[Click here](https://danny-g-smith.github.io/Matching-Game/) to load the game into your browser.
+
+The trick to this game it to remember the cards or icons and match them in as quickly as you can.
+
+Here is what you do:
+
+- Flip the first card to see what it is.
+- Flip the second card, you need to do this fairly quickly.
+    - If the cards match, both cards stay exposed so you can see them.
+    - If the cards do not match, both will flip back the way they were.
+- Keep going until you win or run out of time.
+
+## Game Overview
+
+The game has a deck with 8 pairs of cards, for a total of 16 cards.  When you start a new game, assume:
+
+- Everything is set to zero, except the clock which has 120 seconds.
+- The cards have been shuffled so that no two games are the same.
+
+    
+### Scoring
+
+- The player wins points when:
+    - Matching a pair of cards awards 100 points
+
+## Credits
+- CSS keyframes and animation are from [animate.css](https://github.com/daneden/animate.css) by [Daniel Eden](https://github.com/daneden)
+- The timer code came from [www.sitepoint.com](https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/)
+- The shuffle function came from [stackoverflow.com](http://stackoverflow.com/a/2450976)
+- A significant amount of the logic came from a youtube presentation `FEND P3: Memory Game with Mike Wales` [youtube.com](https://www.youtube.com/watch?v=_rUH-sEs68Y) 
+
+
+## License 
+
+GNU General Public License 2.0+
---
+++ gulpfile.js
@@ -0,0 +1,195 @@
+// Require our dependencies
+const babel       = require('gulp-babel');
+const concat      = require('gulp-concat');
+const cssnano     = require('gulp-cssnano');
+const eslint      = require('gulp-eslint');
+const gulp        = require('gulp');
+const gutil       = require('gulp-util');
+const notify      = require('gulp-notify');
+const plumber     = require('gulp-plumber');
+const rename      = require('gulp-rename');
+const uglify      = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+const reload      = browserSync.reload;
+
+// Set assets paths.
+const paths = {
+   'css': ['app/css/style.css', 'app/css/screen.css', 'app/css/modal.css', 'app/css/keyframes.css'],
+   'scripts': [
+      'app/js/app.js',
+      'app/js/engine.js',
+      'app/js/resources.js',
+      'app/js/views/View.js',
+      'app/js/players/Enemy.js',
+      'app/js/players/Player.js'
+   ]
+};
+
+const htmlWatch = 'app/*.html';
+
+const destFolder = 'dist/';
+
+/**
+ * Handle errors and alert the user.
+ */
+function handleErrors() {
+   const args = Array.prototype.slice.call(arguments);
+
+   notify.onError({
+      'title': 'Task Failed [<%= error.message %>',
+      'message': 'See console.',
+      'sound': 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+   }).apply(this, args);
+
+   gutil.beep(); // Beep 'sosumi' again.
+
+   // Prevent the 'watch' task from stopping.
+   this.emit('end');
+}
+
+/**
+ * Concatenate and transform JavaScript.
+ *
+ * https://www.npmjs.com/package/gulp-concat
+ * https://github.com/babel/gulp-babel
+ */
+gulp.task('css:concat', () = >
+gulp.src(paths.css)
+
+// Deal with errors.
+   .pipe(plumber(
+      {'errorHandler': handleErrors}
+   ))
+
+   // Concatenate partials into a single script.
+   .pipe(concat('style.css'))
+
+   // Save the file.
+   .pipe(gulp.dest(destFolder))
+   .pipe(browserSync.stream())
+)
+;
+
+/**
+ * Minify and optimize style.css.
+ *
+ * https://www.npmjs.com/package/gulp-cssnano
+ */
+gulp.task('cssnano', ['css:concat'], () = >
+gulp.src(destFolder + 'style.css')
+   .pipe(plumber({'errorHandler': handleErrors}))
+   .pipe(cssnano({
+      'safe': true // Use safe optimizations.
+   }))
+   .pipe(rename('style.min.css'))
+   .pipe(gulp.dest(destFolder))
+   .pipe(browserSync.stream())
+)
+;
+
+/**
+ * Concatenate and transform JavaScript.
+ *
+ * https://www.npmjs.com/package/gulp-concat
+ * https://github.com/babel/gulp-babel
+ */
+gulp.task('concat', () = >
+gulp.src(paths.scripts)
+
+// Deal with errors.
+   .pipe(plumber(
+      {'errorHandler': handleErrors}
+   ))
+
+   // Convert ES6+ to ES2015.
+   .pipe(babel({
+      presets: ['ES2015']
+   }))
+
+   // Concatenate partials into a single script.
+   .pipe(concat('app.js'))
+
+   // Save the file.
+   .pipe(gulp.dest(destFolder))
+   .pipe(browserSync.stream())
+)
+;
+
+/**
+ * Minify compiled JavaScript.
+ *
+ * https://www.npmjs.com/package/gulp-uglify
+ */
+gulp.task('uglify', ['concat'], () = >
+gulp.src(destFolder + 'app.js')
+   .pipe(plumber({'errorHandler': handleErrors}))
+   .pipe(rename({'suffix': '.min'}))
+
+   // Convert ES6+ to ES2015.
+   .pipe(babel({
+      presets: ['ES2015']
+   }))
+   .pipe(uglify({
+      'mangle': false
+   }))
+   .pipe(gulp.dest(destFolder))
+   .pipe(browserSync.stream())
+)
+;
+
+/**
+ * JavaScript linting.
+ *
+ * https://www.npmjs.com/package/gulp-eslint
+ */
+gulp.task('js:lint', () = >
+gulp.src(['app/js/*.js'])
+   .pipe(eslint())
+   .pipe(eslint.format())
+   .pipe(eslint.failAfterError())
+   .pipe(browserSync.stream())
+)
+;
+
+/**
+ * Process tasks and reload browsers on file changes.
+ *
+ * https://www.npmjs.com/package/browser-sync
+ */
+gulp.task('browser-sync', function () {
+   browserSync.init({
+      server: {
+         baseDir: 'app/',
+      }
+   });
+});
+
+
+gulp.task('watch', function () {
+   browserSync.init({
+      server: {
+         serveStaticOptions: {
+            extensions: ['html']
+         },
+         baseDir: 'app/',
+         index: 'index.html'
+      }
+   });
+
+   // Run tasks when files change.
+   gulp.watch(paths.css, ['styles']);
+   gulp.watch(htmlWatch, reload);
+   gulp.watch(paths.scripts, ['scripts']);
+});
+
+gulp.task('reload', function () {
+   browserSync.reload;
+});
+
+/**
+ * Create individual tasks.
+ */
+gulp.task('scripts', ['uglify']);
+gulp.task('styles', ['cssnano']);
+gulp.task('lint', ['js:lint']);
+gulp.task('default', ['styles', 'scripts', 'watch', 'reload']);
