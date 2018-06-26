eeff6324ec1eb3911c926dc8d612988c75662078
Danny G Smith <dgs@campfirepixels.com>
2018-06-25 19:53:47 -0500

    got styles compiling ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,24 @@
-//const VERSION = '0.0.1';
-
 // Require our dependencies
 const autoprefixer = require('autoprefixer');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
-const cssnano = require('gulp-cssnano');
+//const cssnano = require('gulp-cssnano');
 const del = require('del');
 const eslint = require('gulp-eslint');
 const gulp = require('gulp');
 const gutil = require('gulp-util');
 const mqpacker = require('css-mqpacker');
 const notify = require('gulp-notify');
+const pleeease = require('gulp-pleeease');
 const plumber = require('gulp-plumber');
-const postcss = require('gulp-postcss');
+//const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+//const size = require('gulp-size');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
-
-// Set assets paths.
-// const paths = {
-//    'css': ['app/css/**/*.css'],
-//    'scripts': ['app/js/**/*.js'],
-//    'html': ['app/**/*.html'],
-// };
 
 // Set assets paths.
 const paths = {
@@ -40,6 +33,16 @@ const dist = {
    'css': ['./dist/css/'],
    'html': ['./dist/'],
    'js': ['./dist/js/'],
+};
+
+const pleeeaseOpts = {
+   'in': 'app/scss/style.scss',
+   'out': 'app/css/style.css',
+   'autoprefixer': {'browsers': ['last 2 versions', '> 2%']},
+   'rem': ['18px'],
+   'pseudoElements': true,
+   'mqpacker': true,
+   'minifier': !'devBuild',
 };
 
 const destFolder = 'dist/';
@@ -62,122 +65,110 @@ function handleErrors() {
    this.emit('end');
 }
 
-
-/**
- * Delete style.css and style.min.css before we minify and optimize
- */
-gulp.task('clean:style', () =>
-   del(['style.css', 'style.min.css']),
-   gutil.log(
-      gutil.colors.inverse('Styles are now clean....[clean:style]'))
-);
-
-/**
- * Concatenate and transform JavaScript.
- *
- * https://www.npmjs.com/package/gulp-concat
- * https://github.com/babel/gulp-babel
- */
-gulp.task('css:concat', () =>
-   gulp.src(paths.css)
-
-   // Deal with errors.
-      .pipe(plumber(
-         {'errorHandler': handleErrors}
-      ))
-
-      // Concatenate partials into a single script.
-      .pipe(concat('style.css'))
-
-      // Save the file.
+// compile Sass
+gulp.task('styles', function(done) {
+   del([dist.css + 'style.css', dist.css + 'style.min.css']);
+   gulp.src(paths.scss)
+      .pipe(pleeease())
+      .pipe(rename({
+         extname: '.css',
+      }))
       .pipe(gulp.dest(dist.css))
-);
+      .pipe(rename({
+         suffix: '.min',
+         extname: '.css',
+      }))
+      .pipe(gulp.dest(dist.css));
+   done();
+});
 
-// gulp.task('styles', function(done) {
+gulp.task('maps', function(done) {
+   gulp.src(paths.scss, {base: '.'})
+      .pipe(sourcemaps.init())
+      .pipe(pleeease())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./dist'));
+   done();
+});
+
+//
+// /**
+//  * Concatenate and transform JavaScript.
+//  *
+//  * https://www.npmjs.com/package/gulp-concat
+//  * https://github.com/babel/gulp-babel
+//  */
+// gulp.task('css:concat', () =>
+//    gulp.src(paths.css)
+//
+//    // Deal with errors.
+//       .pipe(plumber(
+//          {'errorHandler': handleErrors}
+//       ))
+//
+//       // Concatenate partials into a single script.
+//       .pipe(concat('style.css'))
+//
+//       // Save the file.
+//       .pipe(gulp.dest(dist.css))
+// );
+//
+// /**
+//  * Compile Sass and run styleheet through PostCSS.
+//  *
+//  * https://www.npmjs.com/package/gulp-sass
+//  * https://www.npmjs.com/package/gulp-postcss
+//  * https://www.npmjs.com/package/gulp-autoprefixer
+//  * https://www.npmjs.com/package/css-mqpacker
+//  */
+// gulp.task('postcss', gulp.series('css:concat'), function(done) {
 //    gulp.src(paths.scss)
-//       .pipe(sass())
-//       .pipe(sass().on('error', sass.logError))
-//       .pipe(autoprefixer({
-//          browsers: ['last 2 versions'],
+//
+//    // Deal with errors.
+//       .pipe(plumber({'errorHandler': handleErrors}))
+//
+//       // Wrap tasks in a sourcemap.
+//       .pipe(sourcemaps.init())
+//
+//       // Compile Sass using LibSass.
+//       .pipe(sass({
+//          'errLogToConsole': true,
+//          'outputStyle': 'expanded',
 //       }))
-//       .pipe(gulp.dest(dist.css));
+//
+//       // Parse with PostCSS plugins.
+//       .pipe(postcss([
+//          autoprefixer({
+//             'browsers': ['last 2 version'],
+//          }),
+//          mqpacker({
+//             'sort': true,
+//          }),
+//       ]))
+//
+//       // Create sourcemap.
+//       .pipe(sourcemaps.write())
+//
+//       // Create style.css.
+//       .pipe(gulp.dest(dist.css))
+//       .pipe(browserSync.stream())
 //    done();
 // });
-
-
-/**
- * Compile Sass and run styleheet through PostCSS.
- *
- * https://www.npmjs.com/package/gulp-sass
- * https://www.npmjs.com/package/gulp-postcss
- * https://www.npmjs.com/package/gulp-autoprefixer
- * https://www.npmjs.com/package/css-mqpacker
- */
-gulp.task( 'postcss', function(done) {
-   gulp.src(paths.scss)
-
-   // Deal with errors.
-      .pipe(plumber({'errorHandler': handleErrors}))
-
-      // Wrap tasks in a sourceemap.
-      .pipe(sourcemaps.init())
-
-      // Compile Sass using LibSass.
-      .pipe(sass({
-         'errLogToConsole': true,
-         'outputStyle': 'expanded',
-      }))
-
-      // Parse with PostCSS plugins.
-      .pipe(postcss([
-         autoprefixer({
-            'browsers': ['last 2 version'],
-         }),
-         mqpacker({
-            'sort': true,
-         }),
-      ]))
-
-      // Create sourcemap.
-      .pipe(sourcemaps.write())
-
-      // Create style.css.
-      .pipe(gulp.dest(dist.css))
-   done();
-});
-
-/**
- * Minify and optimize style.css.
- *
- * https://www.npmjs.com/package/gulp-cssnano
- */
-gulp.task( 'cssnano', gulp.series('postcss'), function(done) {
-   gulp.src(dist.css)
-      //.pipe(plumber({'errorHandler': handleErrors}))
-      .pipe(cssnano({
-         'safe': true, // Use safe optimizations.
-      }))
-      .pipe(rename('style.min.css'))
-      .pipe(gulp.dest(dist.css));
-      // .pipe(browserSync.stream());
-   gutil.log(
-      gutil.colors.inverse('cssnano....'))
-   done();
-});
-
+//
 // /**
 //  * Minify and optimize style.css.
 //  *
 //  * https://www.npmjs.com/package/gulp-cssnano
 //  */
-// gulp.task('cssnano', gulp.series('postcss'), function() {
-//    gulp.src(dist.css + 'style.css')
-//       .pipe(plumber({'errorHandler': handleErrors}))
+// gulp.task('cssnano', gulp.series('postcss'), function(done) {
+//    gulp.src(dist.css)
 //       .pipe(cssnano({
 //          'safe': true, // Use safe optimizations.
-//       }))
-//       .pipe(rename('style.min.css'))
-//       .pipe(gulp.dest(dist.css));
+//       })).on('error', sass.logError)
+//       .pipe(rename(dist.css + 'style.min.css')).on('error', sass.logError)
+//       .pipe(gulp.dest(dist.css)).on('error', sass.logError)
+//       .pipe(browserSync.stream());
+//    done();
 // });
 
 /**
@@ -252,6 +243,7 @@ gulp.task('watch', function() {
  * Create individual tasks.
  */
 gulp.task('scripts', gulp.series('uglify'));
-gulp.task('styles', gulp.series('cssnano'));
+gulp.task('styles', gulp.series('styles'));
+gulp.task('maps', gulp.series('maps'));
 gulp.task('lint', gulp.series('js:lint'));
 gulp.task('default', gulp.series('styles', 'scripts'));
